c75bff7c882be81992128f4b0d84210f621156e2
torong <hi_ryuji@naver.com>
2024-01-23 09:37:31 +0900

    :sparkles: Code kata - 소수 만들기
    소수 판별 함수를 만들고, 3개의 수를 더했을 때 소수가 되는 경우의 개수 구하기

--- 58.js
+++ 58.js
@@ -5,6 +5,7 @@ function isPrime(num) {
   if (num < 2) return false;
 
   for (leti = 2; i * i <= num; i++) {
+    // i <= Math.sqrt(n)도 가능
     if (num % i === 0) return false;
   }
 
@@ -14,5 +15,18 @@ function isPrime(num) {
 // 3개의 수를 더했을 때 소수가 되는 경우의 개수
 function solution(nums) {
   let answer = 0;
+
+  for (let i = 0; i < nums.length - 2; i++) {
+    for (let j = i + 1; j < nums.length - 1; j++) {
+      for (let k = j + 1; k < nums.length; k++) {
+        // 세 수의 합
+        const sum = nums[i] + nums[j] + nums[k];
+
+        // 소수체크
+        answer += isPrime(sum) ? true : false;
+      }
+    }
+  }
+
   return answer;
 }
