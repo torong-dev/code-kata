ad261f934e31b0c763244c9cefc3f51d18edcb4b
torong <hi_ryuji@naver.com>
2024-01-29 09:40:38 +0900

    Feat: 덧칠하기 문제해결

--- 59.js
+++ 59.js
@@ -1,16 +1,21 @@
 // 덧칠하기
-function solution(n, m, section) {
+
+function solution(n, m, sections) {
   let answer = 0;
-  let position = 1; // 롤러 위치
+  let painted = 0; // 페인트로 칠한 부분의 끝 지점
 
-  for (const target of section) {
-    const distance = target - position - 1; // 목표 위치까지의 거리
-
-    if (distance > 0) {
-      answer += Math.ceil(distance / m); // 롤러 사용 횟수
+  {
+    /* 배열 순회 
+    sectionis의 각 요소에 대해 반복
+    현재 요소를 section에 할당하여 코드 블록 내에서 사용 가능
+  */
+  }
+  for (let section of sections) {
+    // 페인트칠한 부분의 끝 지점이 다시 칠해야 하는 구역보다 작으면
+    if (painted < section) {
+      answer++; // 증가
+      painted = section + m - 1;
     }
-
-    position = target;
   }
 
   return answer;
