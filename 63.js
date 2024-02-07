a53004ae07bb3c83bd9889576c0c11a16039c13e
torong <hi_ryuji@naver.com>
2024-02-07 14:30:15 +0900

    :construction: 숫자 짝꿍 알고리즘 푸는중

---
+++ 63.js
@@ -0,0 +1,14 @@
+// 숫자 짝꿍
+function solution(X, Y) {
+  const countX = countDigits(X);
+  const countY = countDigits(Y);
+  let answer = "";
+
+  // 9부터 0까지 역순으로 반복하면서 짝꿍을 찾음
+  for (let digit = 9; digit >= 0; digit--) {
+    // X와 Y에서 최소 등장 횟수 계산
+    // 최소 등장 횟수를 결과에 추가
+  }
+
+  return answer;
+}
