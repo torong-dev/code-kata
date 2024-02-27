e0be50b75d8199ff637fcc7418693d49aeeeb4ac
torong <hi_ryuji@naver.com>
2024-02-27 14:52:29 +0900

    :sparkles: Code kata - 대충 만든 자

--- 66.js
+++ 66.js
@@ -1,34 +1,35 @@
-// 대충 만든 자판
 function solution(keymap, targets) {
-  const answer = [];
+  let answer = [];
 
-  // targets 배열 순회 -> 문자열에 대한 키를 누르는 횟수 계산
-  for (const target of targets) {
-    let totalPressCount = 0;
+  // targets 배열을 순회
+  targets.forEach((target) => {
+    // 현재 문자열의 최소 인덱스 합을 저장할 변수
+    let targetSum = 0;
 
-    // 문자열 순회 -> 누른 키를 찾고 누른 횟수 계산
-    for (const char of target) {
-      let found = false;
+    // 현재 문자열을 한 글자씩 비교
+    for (let i = 0; i < target.length; i++) {
+      // 최소 인덱스 값을 무한대로 초기화
+      let minIndex = Infinity;
 
-      // 배열 순회 -> 현재 문자 찾기
-      for (const key of keymap) {
-        const index = key.indexOf(char);
+      // keymap 배열을 순회하면서 현재 글자의 인덱스 찾기
+      keymap.forEach((row) => {
+        let columnIndex = row.indexOf(target[i]);
 
-        if (index !== -1) {
-          totalPressCount += index + 1;
-          found = true;
-          break;
+        // 현재 글자가 keymap에 있으면 최소값 갱신
+        if (columnIndex > -1) {
+          minIndex = Math.min(minIndex, columnIndex + 1);
         }
-      }
+      });
 
-      if (!found) {
-        totalPressCount = -1;
-        break;
-      }
+      // 각 문자에 대한 최소 인덱스의 합을 누적
+      targetSum += minIndex;
     }
 
-    answer.push(totalPressCount);
-  }
+    // 각 문자열에 대한 최소 인덱스의 합을 결과 배열에 저장
+    answer.push(targetSum);
+  });
 
+  // Infinity를 -1로 변경
+  answer = answer.map((sum) => (sum === Infinity ? -1 : sum));
   return answer;
 }
