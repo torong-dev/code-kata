6eebb283f1956f02db9b1a55e916f8b422b21989
torong <hi_ryuji@naver.com>
2024-03-01 14:14:12 +0900

    :sparkles: Code kata - 둘만의 암호

--- 67.js
+++ 67.js
@@ -9,6 +9,21 @@ function solution(s, skip, index) {
     let convert = c.charCodeAt(0);
     for (let i = 0; i < index; i++) {
       convert++;
+
+      // skipCode에 포함된 경우 해당 알파벳 건너뛰기
+      while (skipCode.includes(convert)) convert++;
+
+      // 알파벳이 'z'를 넘어간 경우 다시 'a'로 돌아가기
+      if (convert == "z".charCodeAt(0) + 1) convert = "a".charCodeAt(0);
+
+      // 건너뛰어야 하는 경우 다시 건너뛰기
+      while (skipCode.includes(convert)) convert++;
     }
+
+    return convert;
   });
+
+  // ASCII 코드 배열을 문자열로 변환
+  answer = String.fromCharCode(...converted);
+  return answer;
 }
