a355da6422899f6a81a1c085e93af7aa63f0846f
torong <hi_ryuji@naver.com>
2024-03-01 15:47:48 +0900

    :construction: 햄버거 만들기 문제푸는중

---
+++ 68.js
@@ -0,0 +1,14 @@
+// 햄버거 만들기
+function solution(ingredient) {
+  let answer = 0;
+  let count = 0;
+  // 스택 배열 선언
+  let temp = [];
+
+  // ingredient 순회
+  for (let i = 0; i < ingredient.length; i++) {
+    temp.push(ingredient[i]);
+  }
+
+  return answer;
+}
