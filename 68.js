6f8ab98ddc282b43ba1715719833f67b3109073b
torong <hi_ryuji@naver.com>
2024-03-04 14:44:43 +0900

    :sparkles: Code kata - 햄버거 만들기

--- 68.js
+++ 68.js
@@ -1,6 +1,5 @@
 // 햄버거 만들기
 function solution(ingredient) {
-  let answer = 0;
   let count = 0;
   // 스택 배열 선언
   let temp = [];
@@ -8,7 +7,15 @@ function solution(ingredient) {
   // ingredient 순회
   for (let i = 0; i < ingredient.length; i++) {
     temp.push(ingredient[i]);
+
+    // 최신값부터 4개씩 값 확인
+    if (temp.slice(-4).join("") == "1231") {
+      count += 1;
+
+      // 1231이 있으면 스택에서 빼기
+      temp.splice(-4);
+    }
   }
 
-  return answer;
+  return count;
 }
